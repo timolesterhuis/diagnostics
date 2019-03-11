7a1dbfe30c6595bb05bfac4cc04bbca312007f14
Timo Lesterhuis <timolesterhuis@gmail.com>
2019-03-11 21:52:02 +0100

    added setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as readme_file:
+    readme = readme_file.read()
+
+with open("requirements.txt", "r") as requirements_file:
+    requirements = requirements_file.read().split("\N")
+
+setup(
+    name="diagnostics",
+    version="0.0.1",
+    author="Timo Lesterhuis",
+    author_email="timolesterhuis@gmail.com",
+    description="A toolbox to analyse diagnostic data!",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://github.com/tim00w/diagnostics/",
+    packages=find_packages(),
+    install_requires=requirements,
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "License :: MIT",
+    ],
+)
