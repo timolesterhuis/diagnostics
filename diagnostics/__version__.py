c8ac5c3766604085864cfa08f90df527d9d6f500
TF Lesterhuis <timo.lesterhuis@ricardo.com>
2019-05-06 08:41:00 +0200

    Bump version: 0.2.2 --> 0.3.0rc

--- diagnostics/__version__.py
+++ diagnostics/__version__.py
@@ -1,4 +1,4 @@
-__version__ = "0.2.2"
+__version__ = "0.3.0rc"
 __version_info__ = tuple([int(i) for i in __version__.split(".")])
 
 __author__ = "Timo Lesterhuis"
--- docs/conf.py
+++ docs/conf.py
@@ -25,7 +25,7 @@ copyright = '2019, Timo Lesterhuis'
 author = 'Timo Lesterhuis'
 
 # The full version, including alpha/beta/rc tags
-release = '0.2.2'
+release = '0.3.0rc'
 
 
 # -- General configuration ---------------------------------------------------
