ad61aacf29553880f67d21008c3260e17ca72ef4
TF Lesterhuis <timo.lesterhuis@ricardo.com>
2019-05-06 09:37:25 +0200

    Bump version: 0.3.1 --> 0.3.2rc

--- docs/conf.py
+++ docs/conf.py
@@ -25,7 +25,7 @@ copyright = '2019, Timo Lesterhuis'
 author = 'Timo Lesterhuis'
 
 # The full version, including alpha/beta/rc tags
-release = '0.3.1'
+release = '0.3.2rc'
 
 
 # -- General configuration ---------------------------------------------------
